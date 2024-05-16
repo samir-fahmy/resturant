2374f0605ef5fa857a0416c3c72a1b0e104123a6
samir sahion <sam_fahmy@hotmail.com>
2024-05-16 17:24:14 +0300

    add table

--- configuration/models.py
+++ configuration/models.py
@@ -1,1 +1,29 @@
 from django.db import models
+
+   
+class Unit(models.Model):    
+   name_lo=models.CharField('الإسم المحلى', max_length=50) 
+   name_fk=models.CharField('الإسم الأجنبى', max_length=50) 
+   codeUnit=models.CharField('رمز الوحده', max_length=50)
+
+class Items_type(models.Model):
+   name_lo=models.CharField('الإسم المحلى', max_length=50) 
+   name_fk=models.CharField('الإسم الأجنبى', max_length=50) 
+
+
+class Store(models.Model):
+   name_lo=models.CharField('الإسم المحلى', max_length=50) 
+   name_fk=models.CharField('الإسم الأجنبى', max_length=50) 
+   is_stop=models.BooleanField(default=True)
+
+class Items(models.Model):
+   unit=models.ForeignKey(Unit,on_delete = models.CASCADE) 
+   items_type=models.ForeignKey(Items_type, on_delete=models.CASCADE)
+   barcode=models.CharField('باركود',max_length=50)
+ 
+
+class story_items(models.Model):
+   qty=models.IntegerField('الكمية')    
+   items=models.ForeignKey(Items, on_delete=models.CASCADE)
+   stor=models.ForeignKey(Store, on_delete=models.CASCADE)
+   date_in=models.DateTimeField('تاريخ الإضافه')
